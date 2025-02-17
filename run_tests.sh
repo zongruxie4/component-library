9be64f62eaf489721a01e40547e427333a7bd963
Leonardo P. Tizzei <ltizzei@br.ibm.com>
2025-02-17 15:18:31 -0300

    fix shell script
    Signed-off-by: Leonardo P. Tizzei <ltizzei@br.ibm.com>

--- run_tests.sh
+++ run_tests.sh
@@ -2,28 +2,30 @@
 
 echo "Running tests"
 
-std_err_file="~/test-terratorch-iterate.err"
+# set standard err file
+STD_ERR_FILE="~/test-terratorch-iterate.err"
 
 # Using the test command
-if [ -e "$std_err_file" ]
+if [ -f "$STD_ERR_FILE" ]
 then
   echo "Standard Err File exists, so it will be removed to create a new one from scratch"
-  rm $std_err_file
+  rm $STD_ERR_FILE
 else
   echo "Standard Err File does not exist, so it will be created"
 fi
 
-std_out_file="~/test-terratorch-iterate.out"
+# set standard out file
+STD_OUT_FILE="~/test-terratorch-iterate.out"
 
 # Using the test command
-if [ -e "$std_out_file" ]
+if [ -f "$STD_OUT_FILE" ]
 then
   echo "Standard out File exists, so it will be removed to create a new one from scratch"
-  rm $std_out_file
+  rm $STD_OUT_FILE
 else
   echo "Standard out File does not exist, so it will be created"
 fi
 
 echo "Submit test job"
 
-jbsub -e ~/test-terratorch-iterate.err -o ~/test-terratorch-iterate.out -m 20G  -c 1+1 -r v100 pytest --cov-report html --cov=benchmark tests/test_benchmark.py
+jbsub -e $STD_ERR_FILE -o $STD_OUT_FILE -m 20G  -c 1+1 -r v100 pytest --cov-report html --cov=benchmark tests/test_benchmark.py
