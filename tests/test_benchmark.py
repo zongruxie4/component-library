cb8167547673af37dc1c64ba72ed00c02dbc55d6
Leonardo P. Tizzei <ltizzei@br.ibm.com>
2025-02-21 23:23:07 -0300

    temporarily remove some test cases
    Signed-off-by: Leonardo P. Tizzei <ltizzei@br.ibm.com>

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -102,11 +102,11 @@ def find_file(directory: str, filename: str):
 @pytest.mark.parametrize(
     "config, continue_existing_experiment, test_models",
     [
-        ("configs/benchmark_v2_template.yaml", False, False),
-        ("configs/benchmark_v2_template.yaml", True, False),
-        ("configs/benchmark_v2_template.yaml", True, True),
-        ("configs/benchmark_v2_template.yaml", False, True),
-        ("configs/dofa_large_patch16_224_upernetdecoder_true.yaml", False, False),
+        # ("configs/benchmark_v2_template.yaml", False, False),
+        # ("configs/benchmark_v2_template.yaml", True, False),
+        # ("configs/benchmark_v2_template.yaml", True, True),
+        # ("configs/benchmark_v2_template.yaml", False, True),
+        # ("configs/dofa_large_patch16_224_upernetdecoder_true.yaml", False, False),
         ("configs/benchmark_v2_simple.yaml", False, False),
     ],
 )
