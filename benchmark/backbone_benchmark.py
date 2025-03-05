022789aed2fba2e42e123939d7401c73635c8149
Leonardo P. Tizzei <ltizzei@br.ibm.com>
2025-03-05 18:54:45 -0300

    initial modification of pyproject toml
    Signed-off-by: Leonardo P. Tizzei <ltizzei@br.ibm.com>

--- benchmark/backbone_benchmark.py
+++ benchmark/backbone_benchmark.py
@@ -1,6 +1,8 @@
 """
 This module contains the high level functions for benchmarking on a single node.
 """
+
+# import argparse
 import os
 import importlib
 from functools import partial
@@ -9,12 +11,10 @@ import mlflow
 import optuna
 import pandas as pd
 import torch
-from jsonargparse import CLI
 from optuna.pruners import HyperbandPruner
 from optuna.samplers import BaseSampler, RandomSampler
 from tabulate import tabulate
 import pickle
-
 from benchmark.benchmark_types import (
     Defaults,
     ParameterBounds,
@@ -24,12 +24,17 @@ from benchmark.benchmark_types import (
 )
 from benchmark.model_fitting import fit_model, fit_model_with_hparams
 from benchmark.repeat_best_experiment import rerun_best_from_backbone
-from benchmark.utils import (check_existing_task_parent_runs,
-                                    check_existing_experiments, unflatten,
-                                    get_logger, sync_mlflow_optuna,
-                                    REPEATED_SEEDS_DEFAULT)
+from benchmark.utils import (
+    check_existing_task_parent_runs,
+    check_existing_experiments,
+    unflatten,
+    get_logger,
+    sync_mlflow_optuna,
+    REPEATED_SEEDS_DEFAULT,
+)
 
 direction_type_to_optuna = {"min": "minimize", "max": "maximize"}
+
 
 def benchmark_backbone_on_task(
     logger,
@@ -52,20 +57,16 @@ def benchmark_backbone_on_task(
     optuna_db_path = f"{optuna_db_path}/{experiment_name}_{experiment_run_id}.db"
 
     task_run_id = sync_mlflow_optuna(
-                            optuna_db_path = optuna_db_path,
-                            storage_uri = storage_uri,
-                            experiment_name = experiment_name,
-                            task_run_id = task_run_id,
-                            task = task,
-                            n_trials = n_trials,
-                            logger = logger
-                            )
+        optuna_db_path=optuna_db_path,
+        storage_uri=storage_uri,
+        experiment_name=experiment_name,
+        task_run_id=task_run_id,
+        task=task,
+        n_trials=n_trials,
+        logger=logger,
+    )
 
-    with mlflow.start_run(
-        run_name=task.name,
-        nested=True,
-        run_id=task_run_id
-    ) as run:
+    with mlflow.start_run(run_name=task.name, nested=True, run_id=task_run_id) as run:
         logger.info(f"starting task run with id: {run.info.run_id}")
         training_spec = combine_with_defaults(task, defaults)
         if "max_epochs" not in training_spec.trainer_args:
@@ -96,9 +97,9 @@ def benchmark_backbone_on_task(
                 training_spec.task.direction
             ],  # in the future may want to allow user to specify this
             pruner=HyperbandPruner(),
-            study_name=task.name, 
-            storage= "sqlite:///{}.db".format(optuna_db_path), 
-            load_if_exists=True
+            study_name=task.name,
+            storage="sqlite:///{}.db".format(optuna_db_path),
+            load_if_exists=True,
         )
 
         objective = partial(
@@ -116,27 +117,31 @@ def benchmark_backbone_on_task(
 
         n_trials = n_trials - len(study.trials)
         for trial in study.trials:
-            if (trial.state == optuna.trial.TrialState.FAIL) | (trial.state == optuna.trial.TrialState.RUNNING):
-                n_trials=n_trials+1
+            if (trial.state == optuna.trial.TrialState.FAIL) | (
+                trial.state == optuna.trial.TrialState.RUNNING
+            ):
+                n_trials = n_trials + 1
 
         study.optimize(
             objective,
             n_trials=n_trials,
             # callbacks=[champion_callback],
-            catch=[torch.cuda.OutOfMemoryError], 
+            catch=[torch.cuda.OutOfMemoryError],
         )
 
         tags = {
             "early_stop_patience": str(training_spec.task.early_stop_patience),
             "partition_name": str(training_spec.task.datamodule.partition),
             "decoder": str(training_spec.task.terratorch_task["model_args"]["decoder"]),
-            "backbone": str(training_spec.task.terratorch_task["model_args"]["backbone"]),
-            "n_trials": str(n_trials)
+            "backbone": str(
+                training_spec.task.terratorch_task["model_args"]["backbone"]
+            ),
+            "n_trials": str(n_trials),
         }
-        mlflow.set_tags(tags) 
+        mlflow.set_tags(tags)
 
         best_params = unflatten(study.best_trial.params)
-        mlflow.log_params(best_params) # unflatten
+        mlflow.log_params(best_params)  # unflatten
         mlflow.log_metric(f"best_{task.metric}", study.best_value)
         return study.best_value, task.metric, best_params
 
@@ -161,6 +166,7 @@ def parse_optimization_space(space: dict | None) -> optimization_space_type | No
             raise ValueError(f"Invalid type for {key}: {value}")
     return parsed_space
 
+
 def benchmark_backbone(
     defaults: Defaults,
     tasks: list[Task],
@@ -177,8 +183,8 @@ def benchmark_backbone(
     bayesian_search: bool = True,
     continue_existing_experiment: bool = True,
     test_models: bool = False,
-    run_repetitions: int = REPEATED_SEEDS_DEFAULT
-    )-> str:
+    run_repetitions: int = REPEATED_SEEDS_DEFAULT,
+) -> str:
     """Highest level function to benchmark a backbone using a single node
 
     Args:
@@ -198,10 +204,10 @@ def benchmark_backbone(
         bayesian_search (bool): Whether to use bayesian optimization for the hyperparameter search. False uses random sampling. Defaults to True.
         run_repetitions (int): Number of times that the experiment will be repeated. Defaults to 1.
     """
-    base = "/".join(storage_uri.split("/")[:-1]) 
+    base = "/".join(storage_uri.split("/")[:-1])
     PATH_TO_JOB_TRACKING = base + "/" + "job_progress_tracking"
     REPEATED_EXP_FOLDER = base + "/" + "repeated_exp_output_mlflow"
-    logger = get_logger(log_folder= f"{base}/job_logs")
+    logger = get_logger(log_folder=f"{base}/job_logs")
     if not os.path.exists(REPEATED_EXP_FOLDER):
         os.makedirs(REPEATED_EXP_FOLDER)
     if not os.path.exists(PATH_TO_JOB_TRACKING):
@@ -209,12 +215,12 @@ def benchmark_backbone(
 
     if backbone_import:
         importlib.import_module(backbone_import)
-    
+
     mlflow.set_tracking_uri(storage_uri)
     mlflow.set_experiment(experiment_name)
 
     if bayesian_search:
-        sampler: BaseSampler | None = None # take the default
+        sampler: BaseSampler | None = None  # take the default
     else:
         sampler = RandomSampler()
 
@@ -230,20 +236,22 @@ def benchmark_backbone(
     run_hpo = True
     task_run_to_id_match = {}
     if continue_existing_experiment:
-        #find status of existing runs, and delete incomplete runs except one with the most complete tasks
+        # find status of existing runs, and delete incomplete runs except one with the most complete tasks
         existing_experiments = check_existing_experiments(
-                                                        logger,
-                                                        storage_uri, 
-                                                        experiment_name, 
-                                                        run_name,
-                                                        backbone,
-                                                        task_names,
-                                                        n_trials
-                                                        )
+            logger,
+            storage_uri,
+            experiment_name,
+            run_name,
+            backbone,
+            task_names,
+            n_trials,
+        )
         if existing_experiments["no_existing_runs"]:
             logger.info("\nStarting new experiment from scratch")
         else:
-            if (existing_experiments["incomplete_run_to_finish"] is not None) and (run_id is None):
+            if (existing_experiments["incomplete_run_to_finish"] is not None) and (
+                run_id is None
+            ):
                 logger.info("Continuing previous experiment parent run")
                 run_id = existing_experiments["incomplete_run_to_finish"]
                 experiment_id = existing_experiments["experiment_id"]
@@ -254,49 +262,56 @@ def benchmark_backbone(
                 finished_run_id = existing_experiments["finished_run"]
                 run_id = existing_experiments["finished_run"]
 
-            #get previously completed tasks 
-            completed_task_run_names, all_tasks_finished, task_run_to_id_match = check_existing_task_parent_runs(
-                                                                                        logger,
-                                                                                        run_id, 
-                                                                                        storage_uri, 
-                                                                                        experiment_name, 
-                                                                                        n_trials)
+            # get previously completed tasks
+            completed_task_run_names, all_tasks_finished, task_run_to_id_match = (
+                check_existing_task_parent_runs(
+                    logger, run_id, storage_uri, experiment_name, n_trials
+                )
+            )
 
-            table_entries_filename = f"{PATH_TO_JOB_TRACKING}/{experiment_name}-{run_id}_table_entries.pkl"
+            table_entries_filename = (
+                f"{PATH_TO_JOB_TRACKING}/{experiment_name}-{run_id}_table_entries.pkl"
+            )
             if os.path.exists(table_entries_filename):
                 with open(table_entries_filename, 'rb') as handle:
                     table_entries = pickle.load(handle)
     else:
         logger.info("Starting new experiment from scratch")
-    
-    #only run hyperparameter optimization (HPO) if there are no experiments with finished HPO 
+
+    # only run hyperparameter optimization (HPO) if there are no experiments with finished HPO
     if run_hpo:
         logger.info("Running hyperparameter optimization")
-        with mlflow.start_run(run_name=run_name, run_id=run_id, description=description) as run:
+        with mlflow.start_run(
+            run_name=run_name, run_id=run_id, description=description
+        ) as run:
             for task in tasks:
-                #only run task if it was not completed before
-                task_run_name=task.name
-                if task_run_name in completed_task_run_names: 
+                # only run task if it was not completed before
+                task_run_name = task.name
+                if task_run_name in completed_task_run_names:
                     logger.info(f"{task_run_name} already completed")
                     continue
                 else:
                     logger.info(f"{task_run_name} not completed. starting now")
-                    
-                task_run_id = task_run_to_id_match[task_run_name] if task_run_name in task_run_to_id_match else None
+
+                task_run_id = (
+                    task_run_to_id_match[task_run_name]
+                    if task_run_name in task_run_to_id_match
+                    else None
+                )
                 best_value, metric_name, hparams = benchmark_backbone_on_task(
-                                                            logger,
-                                                            defaults,
-                                                            task,
-                                                            storage_uri,
-                                                            experiment_name,
-                                                            experiment_run_id = run.info.run_id,
-                                                            task_run_id = task_run_id,
-                                                            optimization_space=optimization_space,
-                                                            n_trials=n_trials,
-                                                            save_models=save_models,
-                                                            sampler=sampler,
-                                                            test_models=test_models,
-                                                        )
+                    logger,
+                    defaults,
+                    task,
+                    storage_uri,
+                    experiment_name,
+                    experiment_run_id=run.info.run_id,
+                    task_run_id=task_run_id,
+                    optimization_space=optimization_space,
+                    n_trials=n_trials,
+                    save_models=save_models,
+                    sampler=sampler,
+                    test_models=test_models,
+                )
                 table_entries.append([task.name, metric_name, best_value, hparams])
                 table_entries_filename = f"{PATH_TO_JOB_TRACKING}/{experiment_name}-{run.info.run_id}_table_entries.pkl"
                 with open(table_entries_filename, 'wb') as handle:
@@ -314,56 +329,54 @@ def benchmark_backbone(
             )
             experiment_id = run.info.experiment_id
 
-        #check completion of HPO for all tasks before proceeding to next stage  
+        # check completion of HPO for all tasks before proceeding to next stage
         existing_experiments = check_existing_experiments(
-                                                        logger,
-                                                        storage_uri, 
-                                                        experiment_name, 
-                                                        run_name,
-                                                        backbone,
-                                                        task_names,
-                                                        n_trials
-                                                        )
+            logger,
+            storage_uri,
+            experiment_name,
+            run_name,
+            backbone,
+            task_names,
+            n_trials,
+        )
         if existing_experiments["finished_run"] is not None:
             finished_run_id = existing_experiments["finished_run"]
         else:
             logger.info("HPO is not complete. Please re-run this experiment")
             raise RuntimeError
 
-    #run repeated experiments
-    logger.info(f"HPO complete. Now running repeated experiments \n\
+    # run repeated experiments
+    logger.info(
+        f"HPO complete. Now running repeated experiments \n\
                 Parent run: {finished_run_id} \n\
                 Experiment name: {experiment_name} \n\
-                ")
-    path_to_final_results = f"{REPEATED_EXP_FOLDER}/{experiment_name}_repeated_exp_mlflow.csv"
-    
-    rerun_best_from_backbone(
-        logger = logger,
-        parent_run_id = finished_run_id,
-        output_path = path_to_final_results, 
-        defaults = defaults,
-        tasks = tasks,
-        experiment_name = experiment_name,
-        storage_uri = storage_uri,
-        tmp_dir = ray_storage_path,
-        backbone_import = backbone_import,
-        run_name = run_name,
-        n_trials = n_trials,
-        ray_storage_path = ray_storage_path,
-        optimization_space = optimization_space,
-        save_models = save_models,
-        description = description,
-        use_ray = False,
-        run_repetitions = run_repetitions
+                "
     )
+    path_to_final_results = (
+        f"{REPEATED_EXP_FOLDER}/{experiment_name}_repeated_exp_mlflow.csv"
+    )
+
+    if run_repetitions >= 1:
+        rerun_best_from_backbone(
+            logger=logger,
+            parent_run_id=finished_run_id,
+            output_path=path_to_final_results,
+            defaults=defaults,
+            tasks=tasks,
+            experiment_name=experiment_name,
+            storage_uri=storage_uri,
+            tmp_dir=ray_storage_path,
+            backbone_import=backbone_import,
+            run_name=run_name,
+            n_trials=n_trials,
+            ray_storage_path=ray_storage_path,
+            optimization_space=optimization_space,
+            save_models=save_models,
+            description=description,
+            use_ray=False,
+            run_repetitions=run_repetitions,
+        )
 
     return experiment_id
 
 
-def main():
-        
-    CLI(benchmark_backbone, fail_untyped=False)
-
-
-if __name__ == "__main__":
-    main()
