822c0b800e8924787288525bfec72b497944cc02
Leonardo P. Tizzei <ltizzei@br.ibm.com>
2025-05-09 15:28:13 -0300

    fix script that runs multiple test cases
    Signed-off-by: Leonardo P. Tizzei <ltizzei@br.ibm.com>

--- run_tests.py
+++ run_tests.py
@@ -7,6 +7,7 @@ from tests.test_benchmark import TEST_CASE_IDS
 def main():
 
     for tc_id in TEST_CASE_IDS:
+        print(f"Running test case {tc_id}")
         stderr_file = f"test_benchmark-{tc_id}.err"
         stdout_file = f"test_benchmark-{tc_id}.out"
 
@@ -24,14 +25,13 @@ def main():
             out_file.unlink(missing_ok=True)
             assert not out_file.exists()
         jbsub = f"jbsub -e {err_file} -o {out_file} -m 40G -c 1+1 -r v100 pytest --cov-report html --cov=benchmark tests/test_benchmark.py::test_run_benchmark[{tc_id}]"
-        print(f"Submitting: {jbsub}")
         cmd = jbsub.split()
         result = subprocess.run(cmd, capture_output=True)
         if result.returncode == 0:
-            print("Command executed successfully:")
+            print(f"Command executed successfully: {jbsub}")
             msg = "> is submitted to default queue"
-            stdout: str = result.stdout
-            assert isinstance(stdout, str)
+            stdout: str = result.stdout.decode('utf-8')
+            assert isinstance(stdout, str), f"Error! not a str: {stdout=}"
             msg_index = stdout.find(msg)
             assert msg_index > 0
             lt_index = stdout[:msg_index].find(">")
@@ -39,12 +39,14 @@ def main():
             jbinfo = f"jbinfo {job_id}"
             cmd = jbinfo.split()
             result = subprocess.run(cmd, capture_output=True)
-            stdout = result.stdout
+            stdout: str = result.stdout.decode('utf-8')
             while result.returncode == 0 and "RUN" in stdout:
-                time.sleep(30)
+                time.sleep(60)
                 result = subprocess.run(cmd, capture_output=True)
-
+                stdout: str = result.stdout.decode('utf-8')
+                
         else:
+            print(jbsub)
             print("Command failed with error code:", result.returncode)
             print("stderr:", result.stderr)
 
