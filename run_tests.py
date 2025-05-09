704da0f48ce2bf052aadc6b11623b66ed7a245c0
Leonardo P. Tizzei <ltizzei@br.ibm.com>
2025-05-09 15:16:09 -0300

    submit one job per test case to avoid exceeding the maximum job time
    Signed-off-by: Leonardo P. Tizzei <ltizzei@br.ibm.com>

--- run_tests.py
+++ run_tests.py
@@ -1,35 +1,49 @@
 import subprocess
 from pathlib import Path
+import time
 from tests.test_benchmark import TEST_CASE_IDS
-
-STD_ERR_FILE = "test-terratorch-iterate.err"
-STD_OUT_FILE = "test-terratorch-iterate.out"
 
 
 def main():
-    err_file = Path.home() / STD_ERR_FILE
-    if err_file.exists():
-        print(f"Delete file {err_file}")
-        err_file.unlink(missing_ok=True)
-        assert not err_file.exists()
-    out_file = Path.home() / STD_OUT_FILE
 
-    if out_file.exists():
-        print(f"Delete file {out_file}")
-        out_file.unlink(missing_ok=True)
-        assert not out_file.exists()
     for tc_id in TEST_CASE_IDS:
+        stderr_file = f"test_benchmark-{tc_id}.err"
+        stdout_file = f"test_benchmark-{tc_id}.out"
+
+        err_file = Path.home() / stderr_file
+        # delete file if it exists
+        if err_file.exists():
+            print(f"Delete file {err_file}")
+            err_file.unlink(missing_ok=True)
+            assert not err_file.exists()
+        out_file = Path.home() / stdout_file
+
+        # delete file if it exists
+        if out_file.exists():
+            print(f"Delete file {out_file}")
+            out_file.unlink(missing_ok=True)
+            assert not out_file.exists()
         jbsub = f"jbsub -e {err_file} -o {out_file} -m 40G -c 1+1 -r v100 pytest --cov-report html --cov=benchmark tests/test_benchmark.py::test_run_benchmark[{tc_id}]"
         print(f"Submitting: {jbsub}")
         cmd = jbsub.split()
         result = subprocess.run(cmd, capture_output=True)
         if result.returncode == 0:
             print("Command executed successfully:")
-            msg = "is submitted to default queue"
+            msg = "> is submitted to default queue"
             stdout: str = result.stdout
             assert isinstance(stdout, str)
-            index = stdout.find(msg)
-            
+            msg_index = stdout.find(msg)
+            assert msg_index > 0
+            lt_index = stdout[:msg_index].find(">")
+            job_id = stdout[lt_index + 1 : msg_index]
+            jbinfo = f"jbinfo {job_id}"
+            cmd = jbinfo.split()
+            result = subprocess.run(cmd, capture_output=True)
+            stdout = result.stdout
+            while result.returncode == 0 and "RUN" in stdout:
+                time.sleep(30)
+                result = subprocess.run(cmd, capture_output=True)
+
         else:
             print("Command failed with error code:", result.returncode)
             print("stderr:", result.stderr)
