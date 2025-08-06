825f43220483b6cb251e7562388d0049df8b0a0e
Leonardo P. Tizzei <ltizzei@br.ibm.com>
2025-08-06 14:35:43 -0300

    track system metrics using mlflow
    Signed-off-by: Leonardo P. Tizzei <ltizzei@br.ibm.com>

--- benchmark/backbone_benchmark.py
+++ benchmark/backbone_benchmark.py
@@ -221,6 +221,8 @@ def benchmark_backbone(
     base = Path(storage_uri).parents[0]
     PATH_TO_JOB_TRACKING = base / "job_progress_tracking"
     REPEATED_EXP_FOLDER = base / "repeated_exp_output_csv"
+    
+    os.environ["MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING"] = "true"
 
     if logger is None:
         logger = get_logger(log_folder=str(base / "job_logs"))
