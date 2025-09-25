daad59ce67fab6293b1dabd154bd773b6301cd29
Leonardo P. Tizzei <ltizzei@br.ibm.com>
2025-09-25 14:55:52 -0300

    fix another bug
    Signed-off-by: Leonardo P. Tizzei <ltizzei@br.ibm.com>

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -10,8 +10,6 @@ CONFIG_FILES = [
     "configs/tests/terratorch-iterate-configs/test_case_02/test_config_util__encoderdecoder_eo_v2_300_model_factory.yaml",
     "configs/tests/terratorch-iterate-configs/test_case_03/terratorch__encoder_decoder_timm_resnet101_model_factory.yaml",
 ]
-# CONTINUE_EXISTING_EXPERIMENT = [True, False]
-# TEST_MODELS = [True, False]
 HPO = [True]
 INPUT_TEST_MAIN = list(
     itertools.product(HPO, CONFIG_FILES)
@@ -21,8 +19,11 @@ INPUT_TEST_MAIN = list(
 def get_test_ids() -> list[str]:
     test_case_ids = list()
     for hpo, config in INPUT_TEST_MAIN:
+        # get the filename
         filename = config.split("/")[-1].replace(".yaml", "")
+        # set test id
         tid = f"{filename}_hpo_{hpo}"
+        # append to list of test ids
         test_case_ids.append(tid)
     return test_case_ids
 
@@ -32,22 +33,9 @@ def get_test_ids() -> list[str]:
     INPUT_TEST_MAIN,
     ids=get_test_ids(),
 )
-
-# terratorch iterate --hpo --config configs/tests/benchmark_v2_simple.yaml
-@pytest.mark.parametrize(
-    "hpo, config",
-    [
-        (
-            True,
-            "configs/tests/terratorch-iterate-configs/test_case_02/test_config_util__encoderdecoder_eo_v2_300_model_factory.yaml",
-        )
-    ],
-)
 def test_main(
     hpo: bool,
     config: str,
-    continue_existing_experiment: bool,
-    test_models: bool,
 ):
     home_dir = Path(__file__).parent.parent.parent
     config_file: Path = home_dir / config
